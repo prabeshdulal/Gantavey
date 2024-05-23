361f961551c5db60ccbafad346b5c12feaa4feba
Mahesh Shrestha <sthamahes85@gmail.com>
2024-05-23 22:52:46 +0545

    improvement many

---
+++ lib/features/Authentication/screens/destination_page.dart
@@ -0,0 +1,8 @@
+class DestinationPage extends StatelessWidget {
+  const DestinationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
